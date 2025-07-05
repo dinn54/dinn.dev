caa84c231212fa6e2157b631c6b8498240082512
dinn54 <dinn543210@gmail.com>
2025-07-05 15:47:00 +0900

    feat : add design system data into tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import Header from "@/features/header";
+// layout.tsx
+import { Inter, Noto_Sans_KR } from 'next/font/google'
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
+const noto = Noto_Sans_KR({
+  variable: '--font-noto',
+})
+
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -26,7 +26,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`relative ${geistSans.variable} ${geistMono.variable} antialiased w-screen h-[clamp(540px,100vh,1200px)] overflow-x-hidden`}
+				className={`relative ${inter.variable} ${noto.variable} antialiased w-screen h-[clamp(540px,100vh,1200px)] overflow-x-hidden`}
 			>
 				{/* <Header /> */}
 				{children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,94 @@ module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      textStroke: {
-        '1': '1px',
-        '2': '2px',
-        '3': '3px',
+      //design system
+      screens:{
+        tab: '26.25rem',   // 420px
+        pc: '80rem',       // 1280px
       },
-      textStrokeColor: {
-        'white': '#ffffff',
-        'black': '#000000',
+      colors:{
+        home: {
+          light: '#f8fbe8',
+          dark: '#3b4933',
+        },
+        about: {
+          light: '#f8fcf4',
+          dark: '#475d52',
+        },
+        projects: {
+          light: '#fefcf9',
+          dark: '#5a5d50',
+        },
+        review: {
+          light: '#f4f6ff',
+          dark: '#454a5f',
+        },
+        button: {
+          green: {
+            light: '#7A8D66',
+            dark: '#627A53',
+          },
+          gold: {
+            light: '#D3BB76',
+            dark: '#8B7A3B',
+          },
+          blue: {
+            light: '#6A7DE7',
+            dark: '#5162BD',
+          },
+        },
+        util: {
+          scrollbar: {
+            blue: {
+              light: '#bac1e6',
+              dark: '#525b91',
+            },
+            gray: {
+              light: '#d9d9d9',
+              dark: '#5c5c5c',
+            },
+          },
+          input: {
+            light: '#f3f4f6',
+            dark: '#5c5c5c',
+            text: '#B5BBC4'
+          },
+        },
+      },
+      fontFmaily: {
+        inter: ['var(--font-inter)', 'sans-serif'],
+        noto: ['var(--font-noto)', 'sans-serif'],
+        sans: ['var(--font-inter)', 'var(--font-noto)', 'sans-serif'],
+      },
+      fontSize: {
+        '12': '0.75rem',   // 12px
+        '14': '0.875rem',  // 14px
+        '16': '1rem',      // 16px
+        '18': '1.125rem',  // 18px
+        '20': '1.25rem',   // 20px
+        '24': '1.5rem',    // 24px
+        '28': '1.75rem',   // 28px
+        '30': '1.875rem',  // 30px
+        '32': '2rem',      // 32px
+        '36': '2.25rem',   // 36px
+        '38': '2.375rem',  // 38px
+        '40': '2.5rem',    // 40px
+        '52': '3.25rem',   // 52px
+      },
+      fontWeight: {
+        'regular': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+      },
+      lineHeight: {
+        'base': '1.1',
+      },
+      letterSpacing: {
+        'base': '-0.02em'
       },
     },
   },
