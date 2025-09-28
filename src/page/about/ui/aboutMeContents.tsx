7471d2e421d97449ebe3d94fe89dc67915ba36b7
dinn54 <dinn543210@gmail.com>
2025-09-29 00:00:18 +0900

    fix: About me 페이지 내용 수정

--- src/page/about/ui/aboutMeContents.tsx
+++ src/page/about/ui/aboutMeContents.tsx
@@ -55,7 +55,15 @@ export const AboutIntroduction = () => {
   return (
     <div className="pc:gap-3 flex w-full flex-col gap-2 pl-1">
       <H5F className="font-semibold">Introduction</H5F>
-      <B4F className="pl-1">안녕하세요. 주정혁입니다.</B4F>
+      <B4F className="pl-1">
+        React, Typescript 기반 2년차 프론트엔드 개발자 주정혁입니다. <br />
+        <br />웹 클라이언트 서비스 개발과 i18n을 통한 국제화 서비스 제공, 사내
+        관리 도 구 개발을 담당하였으며, 반응형 웹과 사용성 좋은 UX를 제공하는
+        것을 추구 합니다. 팀원들과의 소통에서는 상호 존중을 바탕으로 상대방의
+        의도를 이해 하려 하며, 피드백에 대해서는 열린 마음으로 검토하면서 업무
+        생산성 개선을 위한 노력을 하고 있습니다. 24/7 CS 대응 및 사용자 피드백
+        반영 과정을 통해 프로젝트 운영과 관리에 대해서도 경험을 쌓았습니다.
+      </B4F>
     </div>
   );
 };
@@ -103,7 +111,19 @@ export const AboutWorkExperience = () => {
   return (
     <div className="flex w-full flex-col gap-3">
       <H5F className="font-semibold">Work Experience</H5F>
-      <B3F className="pl-2">캐리버스</B3F>
+      <B3F className="mt-2 pl-2 !font-semibold">캐리버스(Carrieverse)</B3F>
+      <B4F className="mt-2 pl-4">
+        재직 기간: 24.04.15 ~ 25.04.22 <br />
+      </B4F>
+      <B4F className="pl-4">소속: Web3 개발실</B4F>
+
+      <B4F className="pl-4">
+        주요 업무:{" "}
+        <B4F className="!font-bold">
+          HTML 게임 개발, 사내 관리 도구 개발, Web3 교환소 고도화 개발 및 실시간
+          운영 장애 대응
+        </B4F>
+      </B4F>
     </div>
   );
 };
